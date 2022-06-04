034e66d3ac93f46ec6ef3d0a87d59a646cc53b20
coding-gen <sglalonde@gmail.com>
2022-06-03 19:15:19 -0700

    fixed home nav, modified contact content

--- contact.js
+++ contact.js
@@ -6,46 +6,62 @@ function handleSubmit(event) {
   event.preventDefault();
 
   let form = document.querySelector('form');
-  let no_submission = 0
+  let requestedInfo = 0;
+  let contactInfo = 0;
   let output = []
 
   if (form.username.value.length === 0) {
-    output.push(`Username: no submission`);
-    no_submission += 1;
+    output.push(`Name: no submission`);
   }
   else {
     output.push(`Username: ${form.username.value}`);
+    contactInfo += 1;
   }
   if (form.email.value.length === 0) {
     output.push(`Email: no submission`);
-    no_submission += 1;
   }
   else {
     output.push(`Email: ${form.email.value}`);
+    contactInfo += 1;
   }
+
   if (form.newsletter.value.length === 0) {
     output.push(`Newsletter: no submission`)
-    no_submission += 1;
   }
   else {
     output.push(`Newsletter: ${form.newsletter.value}`);
-  }
-  if (form.date.value.length === 0) {
-    output.push(`Date: no submission`);
-    no_submission += 1;
-  }
-  else {
-    output.push(`Date: ${form.date.value}`);
+    requestedInfo += 1;
   }
 
-  if (no_submission < 4) {
+  let interests = $('input[name=gardenCategories]:checked');
+  if (interests.length === 0) {
+    output.push(`Interests: no submission`);
+  }
+  else {
+    interestList = `Interests: `
+    for (let i = 0; i < interests.length - 1; i++) {
+      interestList += interests[i].id + ", "
+    }
+    interestList += interests[interests.length - 1].id
+    output.push(interestList)
+    requestedInfo += 1;
+  }
+  if (form.question.value.length === 0) {
+    output.push(`Questions: no submission`);
+  }
+  else {
+    output.push(`Questions: ${form.question.value}`);
+    requestedInfo += 1;
+  }
+
+  if (requestedInfo > 0 & contactInfo > 0) {
     console.group('======== Form Submission ========');
     for (line of output) {
       console.log(line)
     }
     console.groupEnd();
   } else {
-    alert("You must enter some data to submit this form.");
+    alert("In order to receive a response, you must provide contact information and your interests.");
   }
 
 
